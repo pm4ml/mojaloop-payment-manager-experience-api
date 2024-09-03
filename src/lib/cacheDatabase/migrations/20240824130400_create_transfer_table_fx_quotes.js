9d968f882cc7fd9d66e37672e0ed1061565e15cf
nguni52 <nguni52@gmail.com>
2024-09-03 07:26:37 +0200

    remove comment.

--- src/lib/cacheDatabase/migrations/20240824130400_create_transfer_table_fx_quotes.js
+++ src/lib/cacheDatabase/migrations/20240824130400_create_transfer_table_fx_quotes.js
@@ -14,8 +14,6 @@ const TABLE_NAME = 'fx_transfer';
 
 async function up(knex) {
   return knex.schema.createTable(TABLE_NAME, (table) => {
-
-    // fxquotesResponse data
     table.string('fx_commit_request_id');
     table.string('fx_determining_transfer_id');
     table.string('fx_sender_id_type');
