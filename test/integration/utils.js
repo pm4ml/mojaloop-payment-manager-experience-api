b34f84c0c34896eeec0d5b4806a75a214eda2edb
nguni52 <nguni52@gmail.com>
2024-09-18 09:21:29 +0200

    Integrations Tests

---
+++ test/integration/utils.js
@@ -0,0 +1,75 @@
+/**************************************************************************
+ *  (C) Copyright ModusBox Inc. 2020 - All rights reserved.               *
+ *                                                                        *
+ *  This file is made available under the terms of the license agreement  *
+ *  specified in the corresponding source code repository.                *
+ *                                                                        *
+ *  ORIGINAL AUTHOR:                                                      *
+ *       Yevhen Kyriukha - yevhen.kyriukha@modusbox.com                   *
+ *       Nguni Phakela - nguni @izyane.com                                *
+ **************************************************************************/
+
+const { Logger } = require('@mojaloop/sdk-standard-components');
+const { createMemoryCache } = require('../../src/lib/cacheDatabase/');
+const transferTemplate  = require('./data/transferTemplate.json');
+const lastError  = require('./data/lastError.json');
+const Cache = require('../../src/lib/cacheDatabase/cache');
+
+const createTestDb = async () => {
+  const logger = new Logger.Logger({ context: { app: 'database-cache-unit-tests'}, stringify: () => {}});
+  
+  const db = await createMemoryCache({
+    cacheUrl: 'redis://localhost:6739/0',
+    logger,
+    manualSync: true
+  });
+
+  // await waitForRedis(db.redisCache._getClient(), 10000);
+
+  return db;
+};
+
+const waitForRedis = async (redisClient, timeoutMs = 5000) => {
+  console.log('Waiting for Redis connection...');
+  return new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      reject(new Error('Redis connection timeout'));
+    }, timeoutMs);
+
+    redisClient.ping((err) => {
+      clearTimeout(timeout);
+      if (err) {
+        logger.error('Redis connection failed:', err);
+        reject(err);
+      } else {
+        logger.info('Redis connected successfully');
+        resolve();
+      }
+    });
+  });
+}
+
+const addTransferToCache = async (db, opts) => {
+  const transfer = JSON.parse(JSON.stringify(transferTemplate));
+  transfer.transferId = opts.transferId || transfer.transferId;
+  transfer.amountType = opts.amountType || transfer.amountType;
+  transfer.currency = opts.currency || transfer.currency;
+  transfer.amount = opts.amount || transfer.amount;
+  transfer.transactionType = opts.transactionType || transfer.transactionType;
+  transfer.currentState = opts.currentState || transfer.currentState;
+  if (opts.currentState === 'errored') {
+    transfer.lastError = JSON.parse(JSON.stringify(lastError));
+  }
+  transfer.initiatedTimestamp = opts.initiatedTimestamp || transfer.initiatedTimestamp;
+  if (opts.isPending) {
+    delete transfer.fulfil;
+  } else {
+    transfer.fulfil.body.completedTimestamp = opts.completedTimestamp || transfer.fulfil.body.completedTimestamp;
+  }
+
+  await db.redisCache().set(`transferModel_${opts.transferId}`, JSON.stringify(transfer));
+
+  return transfer;
+};
+
+module.exports = { createTestDb, addTransferToCache };
