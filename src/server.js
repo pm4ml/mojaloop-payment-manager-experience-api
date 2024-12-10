ed6bc46880510bbfd6d71e2831a26531666696e0
v3-Panwar-Ujjwal <v3.panwarujjwal@gmail.com>
2024-12-10 13:40:13 +0530

    fix:lint error src/server.js

--- src/server.js
+++ src/server.js
@@ -8,123 +8,123 @@
  *       Murthy Kakarlamudi - murthy@modusbox.com                         *
  **************************************************************************/
 
-const Koa = require("koa");
-const bodyParser = require("koa-bodyparser");
-const { oas } = require("koa-oas3");
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const { oas } = require('koa-oas3');
 const cors = require('@koa/cors');
 
 // required for authentication and authorisation
-const session = require("koa-session");
-const oidcClient = require("openid-client");
+const session = require('koa-session');
+const oidcClient = require('openid-client');
 
-const http = require("http");
-const path = require("path");
+const http = require('http');
+const path = require('path');
 
-const { Logger } = require("@mojaloop/sdk-standard-components");
+const { Logger } = require('@mojaloop/sdk-standard-components');
 const { CookieStore } = require('@internal/accesscontrol');
 
-const handlers = require("./handlers");
-const middlewares = require("./middlewares");
+const handlers = require('./handlers');
+const middlewares = require('./middlewares');
 
 class Server {
-  constructor(conf, db) {
-    this._conf = conf;
-    this._api = null;
-    this._server = null;
-    this._logger = null;
-    this._db = db;
-  }
-
-  async setupApi() {
-    this._api = new Koa();
-    this._api.keys = this._conf.sessionConfig.sessionKeys;
-    this._logger = await this._createLogger();
-
-    let validator;
-    try {
-      validator = await oas({
-        file: path.join(__dirname, "api.yaml"),
-        endpoint: "/openapi.json",
-        uiEndpoint: "/",
-      });
-    } catch (e) {
-      throw new Error(
-        "Error loading API spec. Please validate it with https://editor.swagger.io/"
-      );
+    constructor(conf, db) {
+        this._conf = conf;
+        this._api = null;
+        this._server = null;
+        this._logger = null;
+        this._db = db;
     }
 
-    this._api.use(async (ctx, next) => {
-      ctx.state = {
-        conf: this._conf,
-        db: this._db,
-      };
-      await next();
-    });
+    async setupApi() {
+        this._api = new Koa();
+        this._api.keys = this._conf.sessionConfig.sessionKeys;
+        this._logger = await this._createLogger();
 
-    this._conf.sessionConfig.store = new CookieStore({
-      logger: this._logger,
-      redisUrl: this._conf.sessionConfig.redisUrl,
-    });
+        let validator;
+        try {
+            validator = await oas({
+                file: path.join(__dirname, 'api.yaml'),
+                endpoint: '/openapi.json',
+                uiEndpoint: '/',
+            });
+        } catch (e) {
+            throw new Error(
+                'Error loading API spec. Please validate it with https://editor.swagger.io/'
+            );
+        }
 
-    await this._conf.sessionConfig.store.connect();
+        this._api.use(async (ctx, next) => {
+            ctx.state = {
+                conf: this._conf,
+                db: this._db,
+            };
+            await next();
+        });
 
-    this._api.use(session(this._conf.sessionConfig, this._api));
+        this._conf.sessionConfig.store = new CookieStore({
+            logger: this._logger,
+            redisUrl: this._conf.sessionConfig.redisUrl,
+        });
 
-    // we need to allow cookies to be forwarded from other origins as this api may not
-    // be served on the same port as the UI
-    // moved this responsibility on Istio side
+        await this._conf.sessionConfig.store.connect();
 
-    if (this._conf.enableCors) {
-      this._api.use(cors({ credentials: true }));
+        this._api.use(session(this._conf.sessionConfig, this._api));
+
+        // we need to allow cookies to be forwarded from other origins as this api may not
+        // be served on the same port as the UI
+        // moved this responsibility on Istio side
+
+        if (this._conf.enableCors) {
+            this._api.use(cors({ credentials: true }));
+        }
+
+        this._api.use(middlewares.createErrorHandler());
+        this._api.use(middlewares.createRequestIdGenerator());
+        this._api.use(middlewares.createLogger(this._logger));
+        this._api.use(bodyParser());
+        if (this._conf.authConfig.enableAuthClient) {
+            this._api.use(
+                await middlewares.createAuthenticator(
+                    oidcClient,
+                    this._conf.authConfig,
+                    this._logger
+                )
+            );
+        }
+        this._api.use(validator);
+        this._api.use(middlewares.createRouter(handlers));
+
+        this._server = this._createServer();
+        return this._server;
     }
 
-    this._api.use(middlewares.createErrorHandler());
-    this._api.use(middlewares.createRequestIdGenerator());
-    this._api.use(middlewares.createLogger(this._logger));
-    this._api.use(bodyParser());
-    if (this._conf.authConfig.enableAuthClient) {
-      this._api.use(
-        await middlewares.createAuthenticator(
-          oidcClient,
-          this._conf.authConfig,
-          this._logger
-        )
-      );
+    async start() {
+        await new Promise((resolve) =>
+            this._server.listen(this._conf.inboundPort, resolve)
+        );
+        this._logger.log(`Serving inbound API on port ${this._conf.inboundPort}`);
     }
-    this._api.use(validator);
-    this._api.use(middlewares.createRouter(handlers));
 
-    this._server = this._createServer();
-    return this._server;
-  }
-
-  async start() {
-    await new Promise((resolve) =>
-      this._server.listen(this._conf.inboundPort, resolve)
-    );
-    this._logger.log(`Serving inbound API on port ${this._conf.inboundPort}`);
-  }
-
-  async stop() {
-    if (!this._server) {
-      return;
+    async stop() {
+        if (!this._server) {
+            return;
+        }
+        await new Promise((resolve) => this._server.close(resolve));
+        console.log('inbound shut down complete');
     }
-    await new Promise((resolve) => this._server.close(resolve));
-    console.log("inbound shut down complete");
-  }
 
-  async _createLogger() {
-    return new Logger.Logger({
-      context: {
-        app: "mojaloop-payment-manager-experience-api",
-      },
-      stringify: Logger.buildStringify({ space: 2 }),
-    });
-  }
+    async _createLogger() {
+        return new Logger.Logger({
+            context: {
+                app: 'mojaloop-payment-manager-experience-api',
+            },
+            stringify: Logger.buildStringify({ space: 2 }),
+        });
+    }
 
-  _createServer() {
-    return http.createServer(this._api.callback());
-  }
+    _createServer() {
+        return http.createServer(this._api.callback());
+    }
 }
 
 module.exports = Server;
